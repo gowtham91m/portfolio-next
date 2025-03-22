5c2adc861301ea70d540bafa0926774c113955b9
ggwm <ggwm@amazon.com>
2025-03-22 08:12:18 -0700

    Add background animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,20 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AiOutlineLinkedin, AiFillGithub, AiOutlineMail, AiOutlineCopy } from 'react-icons/ai';
-
-const experiences = [
-  {
-    year: "2019 - present",
-    position: "Amazon - Software Development Engineer",
-    description:
-      "Fullstack development of Amazon pharmacy signup component. Implemented reusable CDK components to create AWS infrastructure for ETL data pipelines for Amazon support center.",
-    tags: [
-      "Java",
-      "Spring-boot",
-      "React",
-      "CDK",
-      "CICD",
-      "Lambda",
-      "EC2",
-      "DynamoDB",
-    ],
-  },
-  {
-    year: 2017,
-    position: "Latentview Analytics - Data Scientist",
-    description:
-      "Had an exposure to work on digital marketing of Google ad products. Performed statistical analysis on Direct email marketing data to promote products to small and medium businesses.",
-    tags: ["Python", "R", "SQL", "Tableau"],
-  },
-  {
-    year: 2016,
-    position:
-      "Oklahoma State University Alumni Association - Data Analyst Intern",
-    description:
-      "Responsible for managing alumni association database. Develop automation for data management, ETL. Provide data-driven insights on new marketing initiatives. Create Tableau dashboards to monitor business KPIs.",
-    tags: ["Python", "SQL", "Tableau"],
-  },
-  {
-    year: 2015,
-    position: "Oklahoma State University - Masters in Information Systems",
-    description:
-      "Had an opportunity to learn statistics, data analytics, and machine learning.",
-    tags: ["Python", "R", "SQL", "Tableau"],
-  },
-  {
-    year: 2012,
-    position: "Accenture - Software Development Engineer",
-    description:
-      "Developed application logic for a CRM application for new telecom initiatives using Oracle PL/SQL.",
-    tags: ["Oracle", "PL/SQL", "Java"],
-  },
-];
+import ParticlesComponent from './ParticlesComponent'; // Import the new particles component
+import './styles.css'; // Import the CSS file
+import experiences from './experiences';
 
 export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
   const [tooltipText, setTooltipText] = useState("email@gowtham.ai");
   const email = "email@gowtham.ai";
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const handleCopyEmail = () => {
     navigator.clipboard.writeText(email);
@@ -63,8 +23,9 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-[rgb(15,23,42)] flex justify-center">
-      <div className="max-w-[1400px] w-full flex flex-col md:flex-row">
+    <div className="min-h-screen bg-[rgb(15,23,42)] flex justify-center relative">
+      {isClient && <ParticlesComponent />} {/* Use the new particles component */}
+      <div className="max-w-[1400px] w-full flex flex-col md:flex-row container relative z-10">
         <div className="w-full md:w-[30vw] flex flex-col justify-between pt-[100px] pl-4">
           <div className="flex-grow mb-4">
             <h1 className="text-[#b3b3b3] text-4xl">Gowtham Mallikarjuna</h1>
@@ -113,9 +74,7 @@ export default function Home() {
           <div className="pt-12 px-4 pl-5 md:pl-20 text-[#b3b3b3]">
             <h3 className="text-xl font-bold mb-4">Experience</h3>
             {experiences.map((exp, index) => (
-              <div key={index} className="flex mb-5">
-                {/* <div className="bg-[#b3b3b3] h-3 w-3 rounded-full mt-2"></div> */}
-                {/* <div className="w-0.5 bg-[#b3b3b3] ml-1 h-20"></div> */}
+              <div key={index} className="flex mb-5 card">
                 <div className="flex flex-col">
                   <span className="font-bold">
                     {exp.position} <span className="ml-2">({exp.year})</span>
@@ -123,11 +82,7 @@ export default function Home() {
                   <span className="mt-2">{exp.description}</span>
                   <div className="flex flex-wrap mt-2">
                     {exp.tags.map((tag, tagIndex) => (
-                      <span
-                        key={tagIndex}
-                        className="rounded-full px-4 py-1 m-1 text-teal-600 text-xs font-medium"
-                        style={{ backgroundColor: 'rgba(0, 128, 128, 0.1)' }}
-                      >
+                      <span key={tagIndex} className="tag">
                         {tag}
                       </span>
                     ))}
